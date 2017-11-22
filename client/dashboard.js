0ea0f1050b9b5270d36b2bcfd08d98c1e82731e9
redeyes361 <redeyes_361@hotmail.com>
2017-11-22 13:25:20 +0100

    fix wallis bullshit in dashboard->projects() helper

--- client/dashboard.js
+++ client/dashboard.js
@@ -7,8 +7,7 @@ Meteor.subscribe('projects');
 
 Template.dashboard.helpers({
   projects(){
-    console.log(Projects.find({owner: Meteor.user().username}));
-    return Projects.find({ $or: [ { owner: Meteor.user().username }, { participants: Meteor.user().username } ] });
+    return Projects.find({ $or: [ { owner: Meteor.user().username }, { "participants.username": Meteor.user().username } ] });
   },
   isOwner(id){
     var project = Projects.findOne(id);
