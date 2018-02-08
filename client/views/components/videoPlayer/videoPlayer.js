b9372d39e6aaac44a737696eaf34ee3a2a0c03d8
LeclechM <32573436+LeclechM@users.noreply.github.com>
2018-02-08 01:03:14 +0100

    Video player fully destroyed when go back
    The player didn't disappear when return to a previous page. Now is fixed.

--- client/views/components/videoPlayer/videoPlayer.js
+++ client/views/components/videoPlayer/videoPlayer.js
@@ -3,12 +3,20 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import {Projects} from '../../../../lib/collections/Project.js';
 import './videoPlayer.html';
 import '/public/renderers/vimeo.js';
-
+var Player;
 Template.videoPlayer.onRendered(function () {
-    $('video').mediaelementplayer({
+   $('video').mediaelementplayer({
       pluginPath:'/packages/johndyer_mediaelement/',
       success: function (mediaElement, domObject) {
+        Player =mediaElement;
         mediaElement.setSrc((Projects.findOne(Router.current().params._id).url));
       }
     });
+});
+
+Template.videoPlayer.onDestroyed(function(){
+  $('.videoContainer').remove();
+  Player.pause();
+  Player.remove();
+
 });
